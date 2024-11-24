f730339c0ed4958dbdae96a82acb9f1fcec9173d
Syeda Juveria Afreen <sja.affu765@gmail.com>
2024-11-25 02:44:52 +0300

    Update with category functionality

--- pages/header.js
+++ pages/header.js
@@ -2,123 +2,122 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import Link from 'next/link';
 
-export default function Header({ onSearch }) {
+export default function Header({ onSearch, onCategoryChange }) {
+  const [isCategoryDropdownOpen, setCategoryDropdownOpen] = useState(false);
+  const [isAvatarSubmenuOpen, setAvatarSubmenuOpen] = useState(false);
+  const [isFashionSubmenuOpen, setFashionSubmenuOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("Category All");
+  const [searchQuery, setSearchQuery] = useState(""); // For search input
 
+  const handleCategorySelection = (category) => {
+    setSelectedCategory(category);
+    onCategoryChange(category); // Pass the selected category to the parent component
+    setCategoryDropdownOpen(false);
+  };
 
-    const [isCategoryDropdownOpen, setCategoryDropdownOpen] = useState(false);
-    const [isAvatarSubmenuOpen, setAvatarSubmenuOpen] = useState(false);
-    const [isFashionSubmenuOpen, setFashionSubmenuOpen] = useState(false);
-    const [selectedCategory, setSelectedCategory] = useState("Category All");
-    const [searchQuery, setSearchQuery] = useState(""); // For search input
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
-    const handleCategorySelection = (category) => {
-        setSelectedCategory(category); 
-        setCategoryDropdownOpen(false);
-    };
+  const handleSearchClick = () => {
+    onSearch(searchQuery); // Pass search query to parent component
+  };
 
-    const handleSearchChange = (event) => {
-        setSearchQuery(event.target.value);
-    };
-
-    const handleSearchClick = () => {
-        onSearch(searchQuery); // Pass search query to parent component
-    };
-
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/">
-                            <Image src="/images/logo.svg" alt="Logo" width={150} height={50} />
-                        </Link>
-                    </li>
-                    <li className="search-bar">
-                        <input
-                            type="text"
-                            placeholder="Keyword"
-                            className="search-input"
-                            value={searchQuery}
-                            onChange={handleSearchChange} // Update search query state on input change
-                        />
-                        <div class="line">|</div>
-                        <div
-                            className="category-dropdown-wrapper"
-                            onClick={() => setCategoryDropdownOpen(!isCategoryDropdownOpen)}
-                            onMouseEnter={() => setCategoryDropdownOpen(true)}
-                            onMouseLeave={() => setCategoryDropdownOpen(false)}
-                        >
-                            <div className="category-dropdown">{selectedCategory}</div>
-                            {isCategoryDropdownOpen && (
-                                <ul className="category-options">
-                                    <li onClick={() => handleCategorySelection("Category All")}>Category All</li>
-                                    <li
-                                        onMouseEnter={() => setAvatarSubmenuOpen(true)}
-                                        onMouseLeave={() => setAvatarSubmenuOpen(false)}
-                                    >
-                                        Category Avatars
-                                        {isAvatarSubmenuOpen && (
-                                            <ul className="submenu">
-                                                <li onClick={() => handleCategorySelection("Human-like")}>Human-like</li>
-                                                <li onClick={() => handleCategorySelection("Anthro & Furry")}>Anthro & Furry</li>
-                                                <li onClick={() => handleCategorySelection("Robot & Cyborgs")}>Robot & Cyborgs</li>
-                                                <li onClick={() => handleCategorySelection("Others in Avatars")}>Others</li>
-                                                <li onClick={() => handleCategorySelection("All in Avatars")}>All in Avatars</li>
-                                            </ul>
-                                        )}
-                                    </li>
-                                    <li
-                                        onMouseEnter={() => setFashionSubmenuOpen(true)}
-                                        onMouseLeave={() => setFashionSubmenuOpen(false)}
-                                    >
-                                        Category Fashion
-                                        {isFashionSubmenuOpen && (
-                                            <ul className="submenu">
-                                                <li onClick={() => handleCategorySelection("Clothes")}>Clothes</li>
-                                                <li onClick={() => handleCategorySelection("Accessories")}>Accessories</li>
-                                                <li onClick={() => handleCategorySelection("Others in Fashion")}>Others</li>
-                                                <li onClick={() => handleCategorySelection("All in Fashion")}>All in Fashion</li>
-                                            </ul>
-                                        )}
-                                    </li>
-                                </ul>
-                            )}
-                        </div>
-                        <Image src="/images/searchicon.svg" alt="Search Icon" width={50} height={50} onClick={handleSearchClick} />
-                    </li>
-                    <li>
-                        <Image src="/images/filtersearch.svg" alt="Filter Icon" width={50} height={50} />
-                    </li>
-                    <li>
-                        <p>List Your Creation</p>
-                    </li>
-        
-                     {/* Language Dropdown */}
-                    <li className="language-dropdown">
-                        <Image src="/images/lang.svg" alt="Language Icon" width={40} height={40} />
-                        <ul className="language-select">
-                            <li class="lang"> <Image src="/images/english.svg" alt="Language Icon" width={15} height={15} /><p>English</p></li>
-                            <li class="lang"> <Image src="/images/japan.svg" alt="Language Icon" width={15} height={15} /><p>Japanese</p></li>
-                        </ul>
-                    </li>
-
-                    {/* User Dropdown */}
-                    <li className="user-dropdown">
-                        <Image src="/images/usericon.svg" alt="User Icon" width={70} height={70} />
-                        <ul className="user-select">
-                            <li>Sign In</li>
-                            <li>Sign Up</li>
-                            <li>List Your Item</li>
-                            <li>Message to Yuta (The Founder)</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <Link href="/cart">
-                        <Image src="/images/shoppingcart.svg" alt="Shopping Cart Icon" width={50} height={50} />
-                        </Link>
-                    </li>
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <Image src="/images/logo.svg" alt="Logo" width={150} height={50} />
+            </Link>
+          </li>
+          <li className="search-bar">
+            <input
+              type="text"
+              placeholder="Keyword"
+              className="search-input"
+              value={searchQuery}
+              onChange={handleSearchChange} // Update search query state on input change
+            />
+            <div className="line">|</div>
+            <div
+              className="category-dropdown-wrapper"
+              onClick={() => setCategoryDropdownOpen(!isCategoryDropdownOpen)}
+              onMouseEnter={() => setCategoryDropdownOpen(true)}
+              onMouseLeave={() => setCategoryDropdownOpen(false)}
+            >
+              <div className="category-dropdown">{selectedCategory}</div>
+              {isCategoryDropdownOpen && (
+                <ul className="category-options">
+                  <li onClick={() => handleCategorySelection("Category All")}>Category All</li>
+                  <li
+                    onMouseEnter={() => setAvatarSubmenuOpen(true)}
+                    onMouseLeave={() => setAvatarSubmenuOpen(false)}
+                  >
+                    Category Avatars
+                    {isAvatarSubmenuOpen && (
+                      <ul className="submenu">
+                        <li onClick={() => handleCategorySelection("Human-like")}>Human-like</li>
+                        <li onClick={() => handleCategorySelection("Anthro & Furry")}>Anthro & Furry</li>
+                        <li onClick={() => handleCategorySelection("Robot & Cyborgs")}>Robot & Cyborgs</li>
+                        <li onClick={() => handleCategorySelection("Others in Avatars")}>Others</li>
+                        <li onClick={() => handleCategorySelection("All in Avatars")}>All in Avatars</li>
+                      </ul>
+                    )}
+                  </li>
+                  <li
+                    onMouseEnter={() => setFashionSubmenuOpen(true)}
+                    onMouseLeave={() => setFashionSubmenuOpen(false)}
+                  >
+                    Category Fashion
+                    {isFashionSubmenuOpen && (
+                      <ul className="submenu">
+                        <li onClick={() => handleCategorySelection("Clothes")}>Clothes</li>
+                        <li onClick={() => handleCategorySelection("Accessories")}>Accessories</li>
+                        <li onClick={() => handleCategorySelection("Others in Fashion")}>Others</li>
+                        <li onClick={() => handleCategorySelection("All in Fashion")}>All in Fashion</li>
+                      </ul>
+                    )}
+                  </li>
                 </ul>
-            </nav>
-        </div>
-    );
+              )}
+            </div>
+            <Image src="/images/searchicon.svg" alt="Search Icon" width={50} height={50} onClick={handleSearchClick} />
+          </li>
+          <li>
+            <Image src="/images/filtersearch.svg" alt="Filter Icon" width={50} height={50} />
+          </li>
+          <li>
+            <p>List Your Creation</p>
+          </li>
+
+          {/* Language Dropdown */}
+          <li className="language-dropdown">
+            <Image src="/images/lang.svg" alt="Language Icon" width={40} height={40} />
+            <ul className="language-select">
+              <li className="lang"> <Image src="/images/english.svg" alt="Language Icon" width={15} height={15} /><p>English</p></li>
+              <li className="lang"> <Image src="/images/japan.svg" alt="Language Icon" width={15} height={15} /><p>Japanese</p></li>
+            </ul>
+          </li>
+
+          {/* User Dropdown */}
+          <li className="user-dropdown">
+            <Image src="/images/usericon.svg" alt="User Icon" width={70} height={70} />
+            <ul className="user-select">
+              <li>Sign In</li>
+              <li>Sign Up</li>
+              <li>List Your Item</li>
+              <li>Message to Yuta (The Founder)</li>
+            </ul>
+          </li>
+          <li>
+            <Link href="/cart">
+              <Image src="/images/shoppingcart.svg" alt="Shopping Cart Icon" width={50} height={50} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,34 @@ import { useState } from 'react';
 
 export default function Home() {
   const [filteredProducts, setFilteredProducts] = useState([]); // State for filtered products
+  const [selectedCategory, setSelectedCategory] = useState("Category All");
 
   const products = [
-    { id: 1, name: 'Avatars > Human-like', creator: 'SJA', imagestar: '/images/stars.svg', rating: 5.0, price: 10.50, image: '/images/humanlike.svg' },
-    { id: 2, name: 'Avatars > Anthro & Furry', creator: 'Dolami', imagestar: '/images/stars.svg', rating: 5.0, price: 10.50, image: '/images/anthrofurry.svg' },
-    { id: 3, name: 'Avatars > Robot & Cyborgs', creator: 'Developer', imagestar: '/images/stars.svg', rating: 5.0, price: 10.50, image: '/images/robot.svg' },
-    { id: 4, name: 'Fashion > Clothes', creator: 'Gamer', imagestar: '/images/stars.svg', rating: 5.0, price: 10.50, image: '/images/clothes.svg' },
-    { id: 5, name: 'Fashion > Accessories', creator: 'Yuta', imagestar: '/images/stars.svg', rating: 5.0, price: 10.50, image: '/images/accessories.svg' },
+    { id: 1, name: 'Avatars > Human-like', creator: 'SJA', imagestar: '/images/stars.svg', rating: 5.0, price: 10.50, image: '/images/humanlike.svg', category: 'Avatars > Human-like' },
+    { id: 2, name: 'Avatars > Anthro & Furry', creator: 'Dolami', imagestar: '/images/stars.svg', rating: 5.0, price: 10.50, image: '/images/anthrofurry.svg', category: 'Avatars > Anthro & Furry' },
+    { id: 3, name: 'Avatars > Robot & Cyborgs', creator: 'Developer', imagestar: '/images/stars.svg', rating: 5.0, price: 10.50, image: '/images/robot.svg', category: 'Avatars > Robot & Cyborgs' },
+    { id: 4, name: 'Fashion > Clothes', creator: 'Gamer', imagestar: '/images/stars.svg', rating: 5.0, price: 10.50, image: '/images/clothes.svg', category: 'Fashion > Clothes' },
+    { id: 5, name: 'Fashion > Accessories', creator: 'Yuta', imagestar: '/images/stars.svg', rating: 5.0, price: 10.50, image: '/images/accessories.svg', category: 'Fashion > Accessories' },
   ];
+
+  // Function to filter products by category
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+    if (category === "Category All") {
+      setFilteredProducts(products);
+    } else {
+      const filtered = products.filter(product =>
+        product.category.toLowerCase().includes(category.toLowerCase())
+      );
+      setFilteredProducts(filtered);
+    }
+  };
 
   // Function to render main content 3 times in 3 rows
   const renderMainContent = () => {
     const productsToRender = filteredProducts.length > 0 ? filteredProducts : products;
     if (productsToRender.length === 0) {
-        return <div>No products found.</div>; // Show message if no products match the search
+      return <div>No products found.</div>; // Show message if no products match the search
     }
     return Array.from({ length: 3 }).map((_, rowIndex) => (
       <div key={rowIndex} className="row">
@@ -45,15 +59,15 @@ export default function Home() {
     const normalizedQuery = query.trim().toLowerCase();
 
     const filtered = products.filter((product) =>
-        product.name.trim().toLowerCase().includes(normalizedQuery) // Normalize product names
+      product.name.trim().toLowerCase().includes(normalizedQuery) // Normalize product names
     );
 
     setFilteredProducts(filtered);
-};
+  };
 
   return (
     <div>
-      <Header onSearch={handleSearch} />
+      <Header onSearch={handleSearch} onCategoryChange={handleCategoryChange} />
       <main>
         {/* Render main content 3 times */}
         {renderMainContent()}
